b220ebcb1ff58eb5ddd6be8dcf286d5a25fc5a10
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-05-06 21:42:47 +0500

    add card

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
 import PagesHeader from "../components/PagesHeader";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="">
+      <div className="card card-side ml-20 mt-28 bg-base-100 shadow-xl w-5/12 ">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Watch</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/components/PagesHeader.jsx
+++ src/components/PagesHeader.jsx
@@ -2,21 +2,36 @@ import { Link } from "react-router-dom";
 function PagesHeader() {
   return (
     <div className="">
-      <div className="navBar">
+      <div className="navbar flex items-center  justify-around">
         <nav>
-          <ul>
+          <ul className="flex">
             <li>
-              <Link to="/">Home</Link>
+              <Link>
+                <img
+                  src="https://img.freepik.com/premium-photo/colorful-bird-with-multicolored-wings-is-shown_795524-1290.jpg"
+                  alt=""
+                  width="100"
+                  height="100"
+                />
+              </Link>
             </li>
-            <li>
+            {/* <li>
               <Link to="/about">About</Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
 
+        <div className="navbar-end flex gap-5 ms-80">
+          <p>Name</p>
+          <div className="avatar">
+            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src="" />
+            </div>
+          </div>
+        </div>
         <ul>
           <li>
-            <button className="btn btn-primary">Logout</button>
+            <button className="btn btn-primary w-28 ">Logout</button>
           </li>
         </ul>
       </div>
